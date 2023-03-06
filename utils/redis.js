1bc3bfca9413cf791f574780196cb7b8818cdece
Mmaureeny <muthonimaureen4@outlook.com>
2023-03-06 18:14:19 +0000

    MongoDB utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,13 +1,10 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
-/**
- * Represents a Redis client.
- */
+//Represents a Redis client.
+
 class RedisClient {
-  /**
-   * Creates a new RedisClient instance.
-   */
+//Creates a new RedisClient instance.
   constructor() {
     this.client = createClient();
     this.isClientConnected = true;
@@ -20,4 +17,23 @@ class RedisClient {
     });
   }
 
-export const redisClient = RedisClient();
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX)
+      .bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+export const redisClient = new RedisClient();
+export default redisClient;
