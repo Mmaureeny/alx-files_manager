388ca55c138c91dc09a97ae623c157b849ae4960
kevohm <kevinkbet@gmail.com>
2023-03-07 00:48:36 -0800

    fixed eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,8 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
-//Represents a Redis client.
 
 class RedisClient {
-//Creates a new RedisClient instance.
   constructor() {
     this.client = createClient();
     this.isClientConnected = true;
