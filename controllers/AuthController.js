469348b0851b53e496907d4d1afc50b861113110
Mmaureeny <muthonimaureen4@outlook.com>
2023-03-09 22:52:55 -0800

    Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,20 +1,37 @@
-/* eslint-disable import/no-named-as-default */
 import { v4 as uuidv4 } from 'uuid';
+import { ObjectId } from 'mongodb';
+import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import { hashPassword } from './UsersController';
 
-export default class AuthController {
-  static async getConnect(req, res) {
-    const { user } = req;
-    const token = uuidv4();
-
-    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
-    res.status(200).json({ token });
+class AuthController {
+  async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+    const basicAuth = authHeader.split(' ')[1];
+    /* eslint-disable no-undef */
+    const decodedString = atob(basicAuth);
+    /* eslint-disable no-undef */
+    const [email, password] = decodedString.split(':');
+    const user = await dbClient.db.collection('users').findOne({ email });
+    if (!user || hashPassword(password) !== user.password) return res.status(401).send({ error: 'Unauthorized' });
+    const randomString = uuidv4();
+    const key = `auth_${randomString}`;
+    // Store the user id in redis store using key as key
+    await redisClient.set(key, user._id.toString(), 86400);
+    return res.status(200).send({ token: randomString });
   }
 
-  static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
-
-    await redisClient.del(`auth_${token}`);
-    res.status(204).send();
+  async getDisconnect(req, res) {
+    const tokenHeader = req.headers['x-token'];
+    // Get the user id from redis store using token as key
+    const userId = await redisClient.get(`auth_${tokenHeader}`);
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    // Delete the user id from redis store
+    await redisClient.del(`auth_${tokenHeader}`);
+    return res.status(204).send();
   }
 }
+
+const authController = new AuthController();
+export default authController;
